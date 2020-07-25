3d0578ebafacf42d3e4f3ec2d5960ac1500f3975
jjs <tech.wanker@gmail.com>
2020-07-25 00:02:56 -0500

    Initial commit

--- gitinit.sh
+++ gitinit.sh
@@ -1,15 +1,21 @@
 set -x
-current_dir=`basename pwd`
-projectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectprojectproject=`basename pwd`
+PWD=`pwd`
+current_dir=`basename $PWD`
+echo current_dir $current_dir
+project=$current_dir
+echo project $project
 if [ ! $project = $current_dir ] ; then
    echo project $project is not current_dir $current_dir  >&2
 fi 
+
+
  
 #git init
 cp ~/templates/.gitignore .gitignore
 git config --global user.email "tech.wanker@gmail.com"
 git remote add origin ssh://git@github.com/techwanker/$project
 git remote set-url origin ssh://git@github.com/techwanker/$project
+git config --get remote.origin.url
 git remote -v
 git add -A .
 git commit
