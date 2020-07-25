11514a70528677b57610f1999b3cc14e91b5bcda
jjs <tech.wanker@gmail.com>
2020-07-24 23:17:59 -0500

    file > new > project > maven project (template) > spring-boot-blank-archetype put in artifact ID: springfield

---
+++ gitinit.sh
@@ -0,0 +1,16 @@
+set -x
+project=pdsutil
+current_dir=`basename pwd`
+if [ ! $project = $current_dir ] ; then
+   echo project $project is not current_dir $current_dir  >&2
+fi 
+ 
+#git init
+cp ~/templates/.gitignore .gitignore
+git config --global user.email "tech.wanker@gmail.com"
+git remote add origin ssh://git@github.com/techwanker/$project
+git remote set-url origin ssh://git@github.com/techwanker/$project
+git remote -v
+git add -A .
+git commit
+git push -u origin master
